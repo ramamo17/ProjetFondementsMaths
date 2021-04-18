eab512c64fae753f67b265ddf2def9d177054847
ramamo17 <74044308+ramamo17@users.noreply.github.com>
2021-04-18 16:03:45 +0200

    Update Projet2.py
    premier message de léonard

--- Projet2.py
+++ Projet2.py
@@ -1,1 +1,28 @@
 
+import string
+import random
+compteurvotant=1
+nombrecandidat=5
+nombrevotant=10
+V=[]
+listepreference=[]
+A=list(string.ascii_lowercase)
+A=A[0:nombrecandidat]
+print(A)
+
+while compteurvotant<=nombrevotant:
+  V.append("v"+str(compteurvotant))
+  compteurvotant+=1
+print(V)
+
+(for votant in V:
+  listecandidataleatoire=[]
+  while (len(listecandidataleatoire) != nombrecandidat) :
+    candidataleatoire=chr(random.randint(97,nombrecandidat+96))
+    print(candidataleatoire)
+    if candidataleatoire not in listecandidataleatoire:
+      listecandidataleatoire.append(candidataleatoire)
+  print(votant)
+  print(listecandidataleatoire)
+  listepreference.append(listecandidataleatoire)
+listepreference)  >>> voir + bas
