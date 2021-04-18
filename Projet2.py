033bf2e74418a755fa5938400e8943857c433d04
Pinhikipie <58669140+Pinhikipie@users.noreply.github.com>
2021-04-18 16:04:55 +0200

    Update Projet2.py
    debut

--- Projet2.py
+++ Projet2.py
@@ -1,6 +1,27 @@
-
 import string
 import random
+
+def condorcet(x,y):
+  testCond=0
+  for v in listepreference:
+    if (v.index(x)<=v.index(y)):
+      testCond+=1
+  if testCond > nombrevotant/2 :
+    return True
+  else : return False
+
+def winCondorcet():
+  for cand1 in A:
+    cpt = 0
+    for cand2 in A:
+      if (cand1!=cand2):
+        print(cand1, cand2)
+        if cpt == nombrevotant :
+          return cand1
+        if (condorcet(cand1,cand2) != True) :
+          break
+        cpt+=1
+
 compteurvotant=1
 nombrecandidat=5
 nombrevotant=10
@@ -10,19 +31,24 @@ A=list(string.ascii_lowercase)
 A=A[0:nombrecandidat]
 print(A)
 
+
 while compteurvotant<=nombrevotant:
   V.append("v"+str(compteurvotant))
   compteurvotant+=1
 print(V)
 
-(for votant in V:
+for votant in V:
   listecandidataleatoire=[]
-  while (len(listecandidataleatoire) != nombrecandidat) :
-    candidataleatoire=chr(random.randint(97,nombrecandidat+96))
-    print(candidataleatoire)
-    if candidataleatoire not in listecandidataleatoire:
-      listecandidataleatoire.append(candidataleatoire)
+  listebis=[]
+  for element in A:
+    listebis.append(element)
+  while (len(listebis) != 0) :
+    x=random.choice(listebis)
+    listecandidataleatoire.append(x)
+    listebis.remove(x)
   print(votant)
   print(listecandidataleatoire)
   listepreference.append(listecandidataleatoire)
-listepreference)  >>> voir + bas
+
+
+print(winCondorcet())
